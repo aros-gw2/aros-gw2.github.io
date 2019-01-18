798e2a569ca5fc427531d08aacd83c14b8869781
aros-gw2 <46812131+aros-gw2@users.noreply.github.com>
2019-01-18 14:13:11 +0100

    initial commit

---
+++ precache-manifest.5e5e755dec5808faac9a5cd6c0aa5ecd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "81b586697c62a58ee113",
+    "url": "/static/js/main.81b58669.chunk.js"
+  },
+  {
+    "revision": "6aae4e35c70ac1f62996",
+    "url": "/static/js/1.6aae4e35.chunk.js"
+  },
+  {
+    "revision": "81b586697c62a58ee113",
+    "url": "/static/css/main.827da7a1.chunk.css"
+  },
+  {
+    "revision": "6c2b3dbdd3acc10ad3a4edd3f1b85cb9",
+    "url": "/index.html"
+  }
+];
